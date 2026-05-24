839517bd2de8634f91f7a1680deaeb070f08db14
kxfile23-cloud <kxfile23@gmail.com>
2026-05-25 06:01:05 +0700

    Enhance bot with logging and error handling
    Added logging and error handling for the bot application.

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
+import logging
+import traceback
+
 from telegram.ext import Application
+
 from config import BOT_TOKEN
-from database import supabase
+from handlers.start import start_handler
 
-app = Application.builder().token(
-    BOT_TOKEN
-).build()
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
 
-print("BOT RUNNING...")
-print("SUPABASE CONNECTED...")
+print("STARTING BOT...")
 
-app.run_polling()
+try:
+
+    app = Application.builder().token(
+        BOT_TOKEN
+    ).build()
+
+    print("APPLICATION SUCCESS")
+
+    app.add_handler(start_handler)
+
+    print("HANDLER SUCCESS")
+
+    print("BOT RUNNING...")
+    print("SUPABASE CONNECTED...")
+
+    app.run_polling(
+        drop_pending_updates=True
+    )
+
+except Exception as e:
+
+    print("\nERROR DETECTED:\n")
+
+    traceback.print_exc()
+
+    print("\nERROR MESSAGE:")
+    print(str(e))
