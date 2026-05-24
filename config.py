700fb9fbcc272d43b6deb2448962d5f9cec269fa
kxfile23-cloud <kxfile23@gmail.com>
2026-05-25 00:49:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,13 +8,25 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID"))
-LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID"))
+DATABASE_CHANNEL_ID = int(
+    os.getenv("DATABASE_CHANNEL_ID")
+)
 
-FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")
-UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL")
+LOG_CHANNEL_ID = int(
+    os.getenv("DATABASE_CHANNEL_ID")
+)
 
-BOT_USERNAME = os.getenv("BOT_USERNAME")
+FORCE_SUB_CHANNEL = os.getenv(
+    "FORCE_SUB_CHANNEL"
+)
+
+UPDATE_CHANNEL = os.getenv(
+    "UPDATE_CHANNEL"
+)
+
+BOT_USERNAME = os.getenv(
+    "BOT_USERNAME"
+)
 
 ADMIN_IDS = list(
     map(
