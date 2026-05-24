7a6f471cca9718a73f67e4805f6cbe9a7f2ce7bf
kxfile23-cloud <kxfile23@gmail.com>
2026-05-25 05:37:57 +0700

    Refactor app initialization and add Supabase import

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from telegram.ext import Application
 from config import BOT_TOKEN
+from database import supabase
 
-app = Application.builder().token(BOT_TOKEN).build()
+app = Application.builder().token(
+    BOT_TOKEN
+).build()
 
 print("BOT RUNNING...")
+print("SUPABASE CONNECTED...")
 
 app.run_polling()
